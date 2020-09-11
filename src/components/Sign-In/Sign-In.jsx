6165cfa0e11865243e766974eda7d67ca1fe85ec
John Trujillo <amriikk@gmail.com>
2020-09-11 02:26:05 -0700

    Create Sign-In class component and add styles

---
+++ src/components/Sign-In/Sign-In.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import FormInput from '../Form-Input/Form-Input';
+
+import './Sign-In.scss';
+
+class SignIn extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        this.setState({email: '', password:''})
+    }
+
+    handleChange = e => {
+        const { value, name } = e.target;
+
+        this.setState({ [name]: value })
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>I already have an account.</h2>
+                <span>Please sign in with your email and password</span>
+                
+                <form onSubmit={this.handleSubmit} >
+                    <FormInput 
+                        name='email' 
+                        type='email' 
+                        value={this.state.email} 
+                        handleChange={this.handleChange}
+                        label='email'
+                        required />
+                    <FormInput 
+                        name='password' 
+                        type='password' 
+                        value={this.state.password}
+                        handleChange={this.handleChange}
+                        label='password'
+                        required 
+                    />
+                    <input type='submit' value='Submit Form' />
+                </form>
+            </div>
+        )
+    }
+};
+
+export default SignIn;
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import SignIn from '../../components/Sign-In/Sign-In'
 
 import './LoginPage.scss'
 
 const LoginPage = () => (
     <div className='login'>
-        LOGIN
+        <h2>LOGIN</h2>
+        <SignIn />
     </div>
 )
 
-export default LoginPage
+export default LoginPage;
