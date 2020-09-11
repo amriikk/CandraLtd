3be871f1915191bd31dae7d459cef5501b4969b9
John Trujillo <amriikk@gmail.com>
2020-09-11 01:23:20 -0700

    update header links + login link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,11 +13,11 @@ const Header = () => (
             <Link className='option' to='/shop'>
                 SHOP
             </Link>
-            <Link className='option' to='/brands'>
-                BRANDS
-            </Link>
             <Link className='option' to='/contact'>
                 CONTACT
+            </Link>
+            <Link className='option' to='/login'>
+                LOGIN
             </Link>
         </div>
     </div>
