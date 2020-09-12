678317682496c9b220e8212c0d6e4fb423b77345
John Trujillo <amriikk@gmail.com>
2020-09-12 01:16:28 -0700

    update the state in App.css + change to class component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/Shop/Shop';
 import LoginPage from './pages/LoginPage/LoginPage';
 import Header from './components/Header/Header';
+import { auth } from './Firebase/Firebase.Utils';
 
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/login' component={LoginPage} />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/login' component={LoginPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
