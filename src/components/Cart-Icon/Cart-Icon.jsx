39fc386f7fb39d73b95bcf143572fd9abc3cff4f
John Trujillo <amriikk@gmail.com>
2020-11-19 02:40:39 -0800

    create cart drop down component

--- src/components/Cart-Icon/Cart-Icon.jsx
+++ src/components/Cart-Icon/Cart-Icon.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { ReactComponent as ShoppingIcon } from '../../assets/ShoppingIcon.svg';
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './Cart-Icon.scss';
 
---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CustomBtn from '../Custom-Btn/Custom-Btn';
+import CustomButton from '../Custom-Btn/Custom-Btn';
+
+import './Cart.scss';
+
+const Cart = () => (
+    <div> className='cart-dropdown'
+        <div className='cart-items' />
+        <CustomBtn>GO TO CHECKOUT</CustomBtn>
+    </div>
+)
+
+export default Cart;
