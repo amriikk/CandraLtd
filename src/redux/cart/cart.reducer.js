b451e01f63d3407dc85c6cbcb2a09860df615c7c
John Trujillo <amriikk@gmail.com>
2020-11-21 22:15:56 -0800

    update cart.types redux

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -6,14 +6,14 @@ const INITIAL_STATE = {
 
 const cartReducer = (state= INITIAL_STATE, action) => {
     switch (action.type) {
-        case 'TOGGLE_CART_HIDDEN':
+        case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
                 hidden: !state.hidden
-            }
+            };
         default:
             return state;
     }
-}
+};
 
 export default cartReducer;
