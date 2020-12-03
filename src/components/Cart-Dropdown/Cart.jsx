2356551ab3e7734e5560770491dcf3af954ea8f7
John Trujillo <amriikk@gmail.com>
2020-12-03 00:51:39 -0800

    update cart-dropdown to add cart-items

--- src/components/Cart-Dropdown/Cart.jsx
+++ src/components/Cart-Dropdown/Cart.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux'
 import CustomBtn from '../Custom-Btn/Custom-Btn';
 import CartItem from '../Cart-Item/Cart-Item';
 
 import './Cart.scss';
 
-const Cart = () => (
+const Cart = ( { cartItems } ) => (
     <div className='cart-dropdown'>
-        <div className='cart-items' />
+        <div className='cart-items'>
+            {
+                cartItems.map(cartItem => (
+                <CartItem key={cartItem.id} item={CartItem} /> 
+            ))}
+        </div>
         <CustomBtn>GO TO CHECKOUT</CustomBtn>
     </div>
-)
+);
 
-export default Cart;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
+});
+
+export default connect(mapStateToProps)(Cart);
--- src/components/Cart-Item/Cart-Item.jsx
+++ src/components/Cart-Item/Cart-Item.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './Cart-Item.scss';
 
-const CartItem = ({ item: { imageUrl, price, name } } => (
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
     <div className='cart-item'>
         <img src={ imageUrl } alt="item"/>
         <div className='item-details'>
