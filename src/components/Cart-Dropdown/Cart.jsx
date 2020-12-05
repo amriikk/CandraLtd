1e35c10e2eba0f9ab7f091691ce4946af9fcafde
John Trujillo <amriikk@gmail.com>
2020-12-04 22:13:41 -0800

    update cart-dropdown selector memoization

--- src/components/Cart-Dropdown/Cart.jsx
+++ src/components/Cart-Dropdown/Cart.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect';
 
 import CustomBtn from '../Custom-Btn/Custom-Btn';
 import CartItem from '../Cart-Item/Cart-Item';
@@ -19,8 +20,8 @@ const Cart = ( { cartItems } ) => (
     </div>
 );
 
-const mapStateToProps = state => ({
-    cartItems: selectCartItems(state)
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems
 });
 
 export default connect(mapStateToProps)(Cart);
