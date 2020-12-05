ac558bd8ee46360e7eef062ffe580943ee008c5d
John Trujillo <amriikk@gmail.com>
2020-12-04 21:30:09 -0800

    cart-selector memoization

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,2 +1,20 @@
 import { createSelector } from 'reselect';
 
+const selectCart = state => state.cart;
+
+export const selectCartItems = createSelector(
+    [selectCart],
+    (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems =>
+      cartItems.reduce(
+        (accumalatedQ, cartItem) => 
+            accumalatedQ + cartItem.quantity,
+         0
+      )
+);
+    
+
