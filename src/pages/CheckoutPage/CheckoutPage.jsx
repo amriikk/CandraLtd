75090271203c723e0447a9122ff931b8a3ab3d93
John Trujillo <amriikk@gmail.com>
2020-12-09 23:18:14 -0800

    update import for checkout-item

--- src/pages/CheckoutPage/CheckoutPage.jsx
+++ src/pages/CheckoutPage/CheckoutPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import CheckoutItem from '../../components/Checkout-Item/Checkout-Item';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
