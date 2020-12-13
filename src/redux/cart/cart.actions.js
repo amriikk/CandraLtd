fa0179ed1dfdeb3b9a38a7a89fcd645b0a6e6d4d
John Trujillo <amriikk@gmail.com>
2020-12-12 22:50:33 -0800

    update cart reducer w. new case

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,3 +1,4 @@
+import Cart from '../../components/Cart-Dropdown/Cart';
 import CartActionTypes from './cart.types';
 
 export const toggleCartHidden = () => ({
@@ -6,5 +7,10 @@ export const toggleCartHidden = () => ({
 
 export const addItem = item => ({
     type: CartActionTypes.ADD_ITEM,
+    payload: item
+});
+
+export const clearItemFromCart = item => ({
+    type: CartActionTypes.CLEAR_ITEM_FROM_CART,
     payload: item
 });
