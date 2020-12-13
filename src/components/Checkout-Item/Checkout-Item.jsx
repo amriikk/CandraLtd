6b4a38c4864dc8cc957e6549e472402dbfb2c493
John Trujillo <amriikk@gmail.com>
2020-12-13 00:48:47 -0800

    update arrows for quantity in Checkout-Item

--- src/components/Checkout-Item/Checkout-Item.jsx
+++ src/components/Checkout-Item/Checkout-Item.jsx
@@ -4,7 +4,7 @@ import { clearItemFromCart } from '../../redux/cart/cart.actions';
 
 import './Checkout-Item.scss';
 
-const CheckoutItem = ( { cartItem } ) => {
+const CheckoutItem = ( { cartItem, clearItem } ) => {
     const { name, imageUrl, price, quantity } = cartItem
     return (
     <div className='checkout-item'>
@@ -12,9 +12,13 @@ const CheckoutItem = ( { cartItem } ) => {
             <img src={imageUrl} alt="item"/>
         </div>
         <span className='name'>{name}</span>
-        <span className='quantity'>{quantity}</span>
+        <span className='quantity'>
+            <div className='arrow'>&#10094;</div>
+            <span>{quantity}</span>
+            <div className='arrow' >&#10095;</div>
+        </span>
         <span className='price'>{price}</span>
-        <div className='remove-button'>&#10005;</div>      
+        <div className='remove-button' onClick={ () => clearItem(cartItem) }>&#10005;</div>      
     </div>
 
 )};
