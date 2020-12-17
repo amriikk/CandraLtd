174801e417158a19b388998d83cb4976c5725886
John Trujillo <amriikk@gmail.com>
2020-12-17 00:31:53 -0800

    update all hats imageURL to local assets

--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -9,64 +9,64 @@ const SHOP_DATA = [
       {
         id: 1,
         name: 'Brown Brim',
-        imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
-        // imageUrl: "/images/shop-img/hats/brown-brim.png",
+        // imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+        imageUrl: "/images/shop-img/hats/brown-brim.png",
         price: 25
       },
       {
         id: 2,
         name: 'Blue Beanie',
-        imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
-        // imageUrl: "/images/shop-img/hats/blue-beanie.png",
+        // imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+        imageUrl: "/images/shop-img/hats/blue-beanie.png",
         price: 18
       },
       {
         id: 3,
         name: 'Brown Cowboy',
-        imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
-        // imageUrl: "/images/shop-img/hats/brown-cowboy.png",
+        // imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+        imageUrl: "/images/shop-img/hats/brown-cowboy.png",
         price: 35
       },
       {
         id: 4,
         name: 'Grey Brim',
-        imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
-        // imageUrl: "/images/shop-img/hats/grey-brim.png",
+        // imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
+        imageUrl: "/images/shop-img/hats/grey-brim.png",
         price: 25
       },
       {
         id: 5,
         name: 'Green Beanie',
-        imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
-        // imageUrl: "/images/shop-img/hats/green-beanie.png",
+        // imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
+        imageUrl: "/images/shop-img/hats/green-beanie.png",
         price: 18
       },
       {
         id: 6,
         name: 'Palm Tree Cap',
-        imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
-        // imageUrl: "/images/shop-img/hats/palm-tree-cap.png",
+        // imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
+        imageUrl: "/images/shop-img/hats/palm-tree-cap.png",
         price: 14
       },
       {
         id: 7,
         name: 'Red Beanie',
-        imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
-        // imageUrl: "/images/shop-img/hats/red-beanie.png",
+        // imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
+        imageUrl: "/images/shop-img/hats/red-beanie.png",
         price: 18
       },
       {
         id: 8,
         name: 'Wolf Cap',
-        imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
-        // imageUrl: "/images/shop-img/hats/wolf-cap.png",
+        // imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
+        imageUrl: "/images/shop-img/hats/wolf-cap.png",
         price: 14
       },
       {
         id: 9,
         name: 'Blue Snapback',
-        imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
-        // imageUrl: "/images/shop-img/hats/blue-snapback.png",
+        // imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
+        imageUrl: "/images/shop-img/hats/blue-snapback.png",
         price: 16
       }
     ]
@@ -268,15 +268,15 @@ const SHOP_DATA = [
       {
         id: 34,
         name: 'Jean Long Sleeve',
-        // imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
-        imageUrl: "/images/shop-img/mens/roll-up-jean-shirt.png",
+        imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
+        // imageUrl: "/images/shop-img/mens/roll-up-jean-shirt.png",
         price: 40
       },
       {
         id: 35,
         name: 'Burgundy T-shirt',
-        // imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
-        imageUrl: "/images/shop-img/mens/polka-dot-shirt.png",
+        imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
+        // imageUrl: "/images/shop-img/mens/polka-dot-shirt.png",
         price: 25
       }
     ]
