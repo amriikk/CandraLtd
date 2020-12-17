b7afa1a3fcce0f299498f133b624c50b10cf864f
John Trujillo <amriikk@gmail.com>
2020-12-16 23:44:31 -0800

    create Collections Overview component

---
+++ src/components/Collections-Overview/Collections-Overview.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+const CollectionOverview = ({ collections }) => (
+    <div className='collections-overview'>
+
+    </div>
+)
