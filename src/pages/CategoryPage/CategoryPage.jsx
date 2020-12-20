1681deea446e15bd6e9fe7ab3d426254e0c4dc51
John Trujillo <amriikk@gmail.com>
2020-12-19 23:59:20 -0800

    update CategoryPage with H2 title

---
+++ src/pages/CategoryPage/CategoryPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CollectionItem from '../../components/Collection-Item/Collection-Item';
+
+import './CategoryPage.scss';
+
+const CategoryPage () => (
+    <div className='category'>
+        <h2>CATEGORY PAGE</h2>
+    </div>
+);
+
+export default CategoryPage;
