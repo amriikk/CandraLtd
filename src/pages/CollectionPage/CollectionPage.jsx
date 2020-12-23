df5648593c2aa890f057a33cfb67172a07a642d9
John Trujillo <amriikk@gmail.com>
2020-12-22 23:21:47 -0800

    update collection-page item import

--- src/pages/CollectionPage/CollectionPage.jsx
+++ src/pages/CollectionPage/CollectionPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import CollectionItem from '../../components/Collection-Item/Collection-Item';
+import CollectionItem from '../../components/Collection-Item/Collection-Item';
 import { selectCollection } from '../../redux/shop/shop.selectors';
 import './CollectionPage.scss';
 
@@ -11,7 +11,9 @@ const CollectionPage = ({ collection }) => {
         <h2 className='title'>{ title }</h2>
         <div className='items'>
             {
-                items.map(item )
+                items.map(item => (
+                    <CollectionItem key={item.id} item={item}/>
+                ))
             }
         </div>
     </div>
