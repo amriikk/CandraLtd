8b2beb9eb7f61a769ce1478bf53713f51a66fafa
John Trujillo <amriikk@gmail.com>
2020-12-26 23:27:55 -0800

    udpate Stripe Btn component

--- src/components/Stripe-Btn/Stripe-Btn.jsx
+++ src/components/Stripe-Btn/Stripe-Btn.jsx
@@ -1,2 +1,21 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutBtn = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = key;
+
+    return (
+        <StripeCheckoutBtn
+            label='Pay Now'
+            name='Candra Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token=
+        />
+    )
+}
