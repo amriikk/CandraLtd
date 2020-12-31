d62f6a23bd208b794c14e5ed2e72658bb7b3f05a
John Trujillo <amriikk@gmail.com>
2020-12-30 23:54:23 -0800

    update jackets shop data

--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -142,36 +142,36 @@ const SHOP_DATA = {
       {
         id: 18,
         name: 'Black Jean Shearling',
-        imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
-        // imageUrl: "/images/shop-img/jackets/black-shearling.png",
+        // imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
+        imageUrl: "/images/shop-img/jackets/black-shearling.png",
         price: 125
       },
       {
         id: 19,
         name: 'Blue Jean Jacket',
-        imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
-        // imageUrl: "/images/shop-img/jackets/blue-jean-jacket.png",
+        // imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
+        imageUrl: "/images/shop-img/jackets/blue-jean-jacket.png",
         price: 90
       },
       {
         id: 20,
         name: 'Grey Jean Jacket',
-        imageUrl: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
-        // imageUrl: "/images/shop-img/jackets/grey-jean-jacket.png",
+        // imageUrl: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
+        imageUrl: "/images/shop-img/jackets/grey-jean-jacket.png",
         price: 90
       },
       {
         id: 21,
         name: 'Brown Shearling',
-        imageUrl: 'https://i.ibb.co/s96FpdP/brown-shearling.png',
-        // imageUrl: "/images/shop-img/jackets/brown-shearling.png",
+        // imageUrl: 'https://i.ibb.co/s96FpdP/brown-shearling.png',
+        imageUrl: "/images/shop-img/jackets/brown-shearling.png",
         // price: 165
       },
       {
         id: 22,
         name: 'Tan Trench',
-        imageUrl: 'https://i.ibb.co/M6hHc3F/brown-trench.png',
-        // imageUrl: "/images/shop-img/jackets/brown-trench.png",
+        // imageUrl: 'https://i.ibb.co/M6hHc3F/brown-trench.png',
+        imageUrl: "/images/shop-img/jackets/brown-trench.png",
         price: 185
       }
     ]
