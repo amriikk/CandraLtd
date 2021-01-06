1880198725b61fa19c2ccecc91e41ec0d17e31e6
John Trujillo <amriikk@gmail.com>
2021-01-05 22:40:55 -0800

    update Resource Page

--- src/pages/ResourcePage/ResourcePage.jsx
+++ src/pages/ResourcePage/ResourcePage.jsx
@@ -5,7 +5,7 @@ import './ResourcePage.scss'
 const ResourcePage = () => (
     <div>
         <h1>Resources</h1>
-        <h2>@ojasa_</h2>
+        <h2>Stay connect by following us on @ojasa_</h2>
         <span>Follow us on Instagram @ojasa_</span><br/><br/>
     </div>
 )
